788ec333fb6acd1dbae81e5c1c50cbacc6e5af5b
Hugo <hugo.curtillet@grenoble-inp.org>
2021-11-14 14:57:25 +0100

    cleaning

---
+++ CourseFlow/Backend/controller/courseController.js
@@ -0,0 +1,5 @@
+export default class courseController{
+    setUnitOfWork(unifOfWork){
+        this.unitOfWork = unifOfWork;
+    }
+}
--- CourseFlow/model/user.js
+++ CourseFlow/model/user.js
@@ -3,6 +3,7 @@ class user{
     constructor(id, name, email, lastConnection, idCourseFollowed, reward, idQuestion, idResponses){
         this.id = id;
         this.name = name;
+        this.email = email;
         this.lastConnection = lastConnection;
         this.idCourseFollowed = idCourseFollowed;
         this. reward = reward;
