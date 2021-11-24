6a5355c9a106cf8c83f22c2f5080ee7ed8a352d9
paulher <paulher@kth.se>
2021-11-25 00:20:01 +0100

    Added functionality to get courses Updated firebase to latest version, including making changes to code in authprovider and firebase Added chakra ui

--- courseflow/src/views/Home.js
+++ courseflow/src/views/Home.js
@@ -1,10 +1,65 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-export default function Home() {
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  StyledEmailIcon,
+  StyledPasswordIcon,
+  StyledButton,
+  StyledInput,
+  StyledLink,
+  StyledCard,
+  StyledTitle,
+  StyledCourseButton,
+  StyledForm,
+  StyledCourse,
+} from "./auth/style";
+import { useState } from "react";
+import { CenterContainer } from "../components";
+import { useCourses } from "../firebase";
+import { VStack, Box, Button } from "@chakra-ui/react";
+
+function Home() {
+  const [page, setPage] = useState("firstPage");
+
+  const navigate = useNavigate();
+  function goToProfile() {
+    navigate("/profile");
+  }
+
+  const { data: courses } = useCourses();
+
+  if (courses === undefined) {
+    return null;
+  }
+
+  if (page === "firstPage") {
     return (
-        <div>
-            <h1>Big Welcome</h1>
-            Check <Link to="/profile">Profile</Link>
-        </div>
-    )
+      <div>
+        <CenterContainer>
+          <StyledCard>
+            <StyledTitle>Homepage</StyledTitle>
+          </StyledCard>
+        </CenterContainer>
+        <VStack>
+          {courses.map((course) => (
+            <Button
+              padding="10px"
+              borderColor="red"
+              borderWidth="4px"
+              borderRadius="md"
+              shadow="md"
+              margin="10px"
+              backgroundColor="white"
+              key={course.name}
+            >
+              {course.name}
+            </Button>
+          ))}
+        </VStack>
+
+        <StyledButton onClick={goToProfile}>Profile</StyledButton>
+      </div>
+    );
+  }
 }
+
+export default Home;
--- courseflow/src/views/auth/style.js
+++ courseflow/src/views/auth/style.js
@@ -1,88 +1,88 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { Button, Card, InputGroup, Form, Collapse } from "react-bootstrap";
-import { Envelope, Lock } from 'react-bootstrap-icons';
+import { Envelope, Lock } from "react-bootstrap-icons";
 
 export const StyledCard = styled(Card)`
-    background-color: #E5E5E5;
-    border: none;
-    display: flex;
-    padding: 20px;
-    margin: 20px;
-    max-width: 400px;
-    text-align: center;
-    align-content: center;
-    font-family: Georgia, "Times New Roman", Times, serif;
+  background-color: #e5e5e5;
+  border: none;
+  display: flex;
+  padding: 20px;
+  margin: 20px;
+  max-width: 400px;
+  text-align: center;
+  align-content: center;
+  font-family: Georgia, "Times New Roman", Times, serif;
 `;
 
 export const StyledForm = styled(Form)`
-    align-content: center;
-    text-align: center;
-    font-family: Georgia, "Times New Roman", Times, serif;
+  align-content: center;
+  text-align: center;
+  font-family: Georgia, "Times New Roman", Times, serif;
 `;
 
 export const StyledTitle = styled.h2`
-    text-align: center; 
-    margin-bottom: 30px;
+  text-align: center;
+  margin-bottom: 30px;
 `;
 
 export const StyledLink = styled.div`
-    width: 100%;
-    text-align: center;
-    margin-top: 15px;
-    font-family: Georgia, "Times New Roman", Times, serif;
+  width: 100%;
+  text-align: center;
+  margin-top: 15px;
+  font-family: Georgia, "Times New Roman", Times, serif;
 `;
 
 export const StyledEmailIcon = styled(Envelope)`
-    display: inline-block;
-    height: 36px;
-    width: 20px;
-    margin-right: 10px;
-    text-align: center;
+  display: inline-block;
+  height: 36px;
+  width: 20px;
+  margin-right: 10px;
+  text-align: center;
 `;
 
 export const StyledPasswordIcon = styled(Lock)`
-    display: inline-block;
-    height: 36px;
-    width: 20px;
-    margin-right: 10px;
-    text-align: center;
+  display: inline-block;
+  height: 36px;
+  width: 20px;
+  margin-right: 10px;
+  text-align: center;
 `;
 
 export const StyledButton = styled(Button)`
-    background: #F5F5F5;
-    color: black;
-    border-radius: 30px;
-    width: 150px;
-    margin-bottom: 35px;
-    font-family: Georgia, "Times New Roman", Times, serif;
+  background: #f5f5f5;
+  color: black;
+  border-radius: 30px;
+  width: 150px;
+  margin-bottom: 35px;
+  font-family: Georgia, "Times New Roman", Times, serif;
 `;
 
 export const StyledInput = styled(InputGroup)`
-    width: 100%;
-    border-radius: 30px;
-    margin-bottom: 20px;
-    vertical-align: middle;
-    text-align: center;
-    font-family: Georgia, "Times New Roman", Times, serif;
+  width: 100%;
+  border-radius: 30px;
+  margin-bottom: 20px;
+  vertical-align: middle;
+  text-align: center;
+  font-family: Georgia, "Times New Roman", Times, serif;
 `;
 
 export const StyledIcon = styled.div`
-    display: inline-block;
-    background-color: whitesmoke;
-    height: 40px;
-    width: 40px;
-    border-radius: 50%;
-    text-align: center;
-    line-height: 40px;
-    font-weight: bold;
-    margin-right: 10px;
-    margin-bottom: 20px;
+  display: inline-block;
+  background-color: whitesmoke;
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 40px;
+  font-weight: bold;
+  margin-right: 10px;
+  margin-bottom: 20px;
 `;
 
 export const StyledCollapse = styled(Collapse)`
-    background: whitesmoke;
-    width: 100%;
-    padding: 20px;
-    margin-top: 20px;
-    margin-bottom: 20px;
+  background: whitesmoke;
+  width: 100%;
+  padding: 20px;
+  margin-top: 20px;
+  margin-bottom: 20px;
 `;
