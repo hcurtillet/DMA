3f6d7ad77f8fe8c896d4fc20e3121416ed08efe9
hcurtillet <hugo.curtillet@gmail.com>
2021-11-25 21:12:57 +0100

    cleaning

--- courseflow/README.md
+++
@@ -1,7 +0,0 @@
-# Setup Firebase
-
-1. install firebase
-
-    npm install firebase
-
-2. Create a `.env` file in your root
--- courseflow/src/views/auth/index.js
+++
@@ -1,9 +0,0 @@
-import SignUp from "./SignUp";
-import Login from "./Login";
-import ForgetPassword from "./ForgetPassword";
-
-export { 
-    SignUp,
-    Login, 
-    ForgetPassword,
-};
