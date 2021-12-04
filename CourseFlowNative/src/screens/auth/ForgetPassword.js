b1f9786e66a0d7bee455b5bfa17d68d8b90cffba
SUN-Xiaoying <xiaoyingsun523@gmail.com>
2021-12-04 21:56:32 +0100

    xiao: remove unused import

--- CourseFlowNative/src/screens/auth/ForgetPassword.js
+++ CourseFlowNative/src/screens/auth/ForgetPassword.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView, TouchableOpacity, View, Text, TextInput, Button } from 'react-native';
 import { Logo } from '../../components';
 import{
